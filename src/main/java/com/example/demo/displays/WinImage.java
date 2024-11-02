d251f26bc9ed4c7ba79e82a61850f9e5cf6a7c99
jacesu <jacesu23@gmail.com>
2024-11-02 16:17:50 +0800

    Handle NullPointerException

--- src/main/java/com/example/demo/displays/WinImage.java
+++ src/main/java/com/example/demo/displays/WinImage.java
@@ -3,6 +3,8 @@ package com.example.demo.displays;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public class WinImage extends ImageView
 {
 	
@@ -12,7 +14,7 @@ public class WinImage extends ImageView
 	
 	public WinImage(double xPosition, double yPosition)
 	{
-		this.setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
+		this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
 		this.setVisible(false);
 		this.setFitHeight(HEIGHT);
 		this.setFitWidth(WIDTH);
