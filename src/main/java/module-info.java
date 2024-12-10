dce67b4bd76a79b8111499e6c2130bec9a027a48
jacesu <jacesu23@gmail.com>
2024-12-10 15:20:55 +0800

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,12 @@ module com.example.demo
 
     exports com.example.demo.controller;
     opens com.example.demo.controller to javafx.fxml;
+    opens com.example.demo.levels to javafx.fxml;
+    opens com.example.demo.displays to javafx.fxml;
+    opens com.example.demo.managers to javafx.fxml;
+    opens com.example.demo.entities.behaviors to javafx.fxml;
+    opens com.example.demo.entities.destructibles to javafx.fxml;
+    opens com.example.demo.entities.planes to javafx.fxml;
+    opens com.example.demo.entities.projectiles to javafx.fxml;
+    opens com.example.demo.utility to javafx.fxml;
 }
